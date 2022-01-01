e9d11d8669562ddd0c41d0972182077f96f1b9b1
ghinafarajwati <g.farajwati@yahoo.com>
2022-01-02 02:42:18 +0700

    Refleksi

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import NaviBar from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css"
 import Refleksi from "./page/Refleksi";
-import Module from "./page/Module";
+// import Module from "./page/Module";
 
 function App() {
   return (
     <div className="App">
       <NaviBar />
       <Refleksi></Refleksi>
-      <Module></Module>
+      {/* <Module></Module> */}
     </div>
   );
 }
