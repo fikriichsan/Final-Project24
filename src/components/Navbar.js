59883e4030ae00a08bb72107f1f633ddf2ae7f4d
fikriichsan <fikri.alichsan@gmail.com>
2022-01-02 01:00:35 +0700

    fix navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,12 +7,12 @@ const NaviBar = () => {
     return(
         <Navbar bg="light" expand="lg">
             <Container className="d-flex">
-                <Navbar.Brand style={{ position: "absolute"}}>
+                <Navbar.Brand>
                     ArtMedial
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
-                <Nav>
+                <Nav id="basic-navbar-nav" className="justify-content-center">
                     <Nav.Item>
                         <Nav.Link eventKey="link-1">Home</Nav.Link>
                     </Nav.Item>
