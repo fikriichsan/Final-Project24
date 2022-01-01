8cb5359403a4d577fa485951a416c480447695f1
fikriichsan <fikri.alichsan@gmail.com>
2022-01-02 00:44:56 +0700

    component Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import React from "react";
+import NaviBar from "./components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
     <div className="App">
-      
+      <NaviBar />
     </div>
   );
 }
