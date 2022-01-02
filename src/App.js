6efc500af5eef41275bbf38d7b7c77f27920dac4
ghinafarajwati <g.farajwati@yahoo.com>
2022-01-03 00:13:57 +0700

    modul 6

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from "react";
 import NaviBar from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css"
 // import MelanjutkanKarya from "./page/MelanjutkanKarya";
-// import Refleksi from "./page/Refleksi";
-import Module from "./page/Module";
+import Refleksi from "./page/Refleksi";
+// import Module from "./page/Module";
+import Modul5 from "./page/Modul5"
 
 function App() {
   return (
     <div className="App">
       <NaviBar />
-      <Module></Module>
-      {/* <Refleksi></Refleksi> */}
+      {/* <Module></Module> */}
+      {/* <Modul5></Modul5> */}
+      <Refleksi></Refleksi>
       {/* <MelanjutkanKarya></MelanjutkanKarya> */}
     </div>
   );
---
+++ src/page/Modul5.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import CardSub from "../components/cardSub";
+
+function Modul5() {
+  return (
+    <CardSub
+      content={{
+        title: "Refleksi",
+        list1: "Runtutan Proses Belajar",
+        list2: "Proses Refleksi",
+        list3: "Hasil Refleksi"
+      }}
+    ></CardSub>
+  );
+}
+
+export default Modul5;
