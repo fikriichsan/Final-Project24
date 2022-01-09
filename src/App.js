898f99d593b399d6e0d92c15f9823efe1e8de8bf
jesslynauryn <88038899+jesslynauryn@users.noreply.github.com>
2022-01-09 15:58:29 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import NaviBar from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Modul5 from "./page/Modul5";
+import Modul6 from "./page/Modul6";
 
 function App() {
   return (
     <div className="App">
       <NaviBar />
       <Modul5 />
+      <Modul6 />
     </div>
   );
 }
